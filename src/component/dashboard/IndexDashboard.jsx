1a9551c6926be9d34ff3393531ae66bf82daa4cb
unknown <Systems@SYPULJGMSS93.synechron.com>
2021-11-07 19:58:26 +0530

    hi

---
+++ src/component/dashboard/IndexDashboard.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+
+class IndexDashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h2>DashBoard Page 1</h2>
+      </Fragment>
+    );
+  }
+}
+export default IndexDashboard;
